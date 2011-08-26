b368d35c952dbef08a8a189a845a72092b39c02e
Capi Etheriel <barraponto@gmail.com>
2011-08-26 17:12:58 -0300

    positioning and styling the search form

--- template.php
+++ template.php
@@ -1,5 +1,13 @@
 <?php
 
 function brado_preprocess_page(&$vars) {
+  $vars['breadcrumb'] = FALSE;
+  $vars['primary_local_tasks'] = FALSE;
+  $vars['secondary_local_tasks'] = FALSE;
   drupal_add_css('http://fonts.googleapis.com/css?family=Hammersmith+One', 'external');
+}
+
+function brado_form_search_block_form_alter(&$form, &$form_state) {
+  $form['search_block_form']['#attributes']['placeholder'] = 'Pesquisa';
+  $form['search_block_form']['#size'] = 25;
 }
