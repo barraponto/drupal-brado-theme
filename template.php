fa57ca5036e389f0bf2750cd208f00cbb0135e28
Capi Etheriel <barraponto@gmail.com>
2011-09-14 15:39:37 -0300

    hijacked the title for a copyright attribution

--- template.php
+++ template.php
@@ -20,3 +20,32 @@ function brado_form_search_block_form_alter(&$form, &$form_state) {
   $form['search_block_form']['#attributes']['placeholder'] = 'Pesquisa';
   $form['search_block_form']['#size'] = 25;
 }
+
+function brado_field__field_banner($variables) {
+  $output = '';
+
+  // Render the label, if it's not hidden.
+  if (!$variables['label_hidden']) {
+    $output .= '<div class="field-label"' . $variables['title_attributes'] . '>' . $variables['label'] . ':&nbsp;</div>';
+  }
+
+  // Render the items.
+  $output .= '<div class="field-items"' . $variables['content_attributes'] . '>';
+  foreach ($variables['items'] as $delta => $item) {
+    $classes = 'field-item ' . ($delta % 2 ? 'odd' : 'even');
+    // Get the copyright attribution from the image title
+    $attribution = $item['#item']['title'];
+    // Unset the title
+    $item['#item']['title'] = FALSE;
+    // Wrap the copyright in markup and put it in the output
+    $copyright = '<p class="copyright">' . $attribution . '</p>';
+    $output .= '<div class="' . $classes . '"' . $variables['item_attributes'][$delta] . '>' . drupal_render($item) . $copyright  . '</div>';
+  }
+  $output .= '</div>';
+
+
+  // Render the top-level DIV.
+  $output = '<div class="' . $variables['classes'] . '"' . $variables['attributes'] . '>' . $output . '</div>';
+
+  return $output;
+}
